4f9cd924e1ad4fac695be7ef2797453d13ff6716
SoaadMahmoud <37635397+SoaadMahmoud@users.noreply.github.com>
2019-07-08 15:00:45 +0200

    Update securitycenter.tf

--- securitycenter.tf
+++ securitycenter.tf
@@ -1,11 +1,11 @@
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "tfex-security-workspace"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+resource "azurerm_log_analytics_workspace" "Akwa" {
+  name                = "Akwa-security-workspace"
+  location            = "${azurerm_resource_group.Akwa.location}"
+  resource_group_name = "${azurerm_resource_group.Akwa.name}"
   sku                 = "PerGB2018"
 }
 
-resource "azurerm_security_center_workspace" "example" {
+resource "azurerm_security_center_workspace" "Akwa" {
   scope        = "/subscriptions/00000000-0000-0000-0000-000000000000"
-  workspace_id = "${azurerm_log_analytics_workspace.example.id}"
+  workspace_id = "${azurerm_log_analytics_workspace.Akwa.id}"
 }
